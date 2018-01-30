ef7355529700e6085b628dd44641b1a0e4fc11f4
Oscar Azofeifa <oscar.azofeifa@gorillalogic.com>
2018-01-30 14:24:04 -0600

    Build Json Capabilities from Property File

--- src/main/java/framework/common/Utilities.java
+++ src/main/java/framework/common/Utilities.java
@@ -1,7 +1,69 @@
 package framework.common;
 
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by pablomeseguer on 1/29/18.
  */
 public class Utilities {
+
+    public String getCapabilities(String name) throws IOException {
+        String initialRead = "";
+        String finalRead = "";
+        String strLine;
+
+        // Open the file
+        FileInputStream fstream = new FileInputStream(name);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+
+        //Read File Line By Line
+        while ((strLine = br.readLine()) != null) {
+            if (strLine != null) {
+                if (strLine.contains("#") && strLine.contains("iOS")) {
+                    initialRead = "{";
+                } else if (strLine.contains("#")) {
+                    initialRead = initialRead + "},{";
+                } else {
+                    initialRead = initialRead + "\"" + strLine.replace('=', ':') + ",";
+                }
+            }
+        }
+
+        String[] parts = initialRead.split(",");
+
+        for (int i = 0; i < parts.length - 1; i++) {
+
+            if (parts[i].contains("url")) {
+                parts[i] = parts[i].replace("url:", "url\":");
+                parts[i].trim();
+
+                finalRead = finalRead + parts[i] + ",";
+
+            } else if (parts[i + 1].contains("}") || i == parts.length - 2) {
+                parts[i] = parts[i].replace(":", "\":");
+                finalRead = finalRead + parts[i];
+
+            } else {
+                parts[i] = parts[i].replace(":", "\":");
+                finalRead = finalRead + parts[i] + ",";
+            }
+        }
+        //Add last value due to +1 in the Split.
+        parts[parts.length - 1] = parts[parts.length - 1].replace(":", "\":");
+        finalRead = finalRead + "," + parts[parts.length - 1];
+
+        //Json Formatting
+        finalRead = finalRead.replace(",\"}", "}");
+        finalRead = finalRead.replace("{\",", "{");
+        finalRead = "{\"capabilities\": [" + finalRead + "}]}";
+
+        //Close the input stream
+        br.close();
+
+        return finalRead;
+    }
 }
--- src/main/java/framework/test.java
+++ src/main/java/framework/test.java
@@ -1,6 +1,9 @@
 package framework;
 
+import framework.common.Utilities;
 import framework.config.Driver;
+
+import java.io.IOException;
 
 /**
  * Created by pablomeseguer on 1/29/18.
@@ -9,14 +12,20 @@ public class test {
 
 
 
-        public static void main(String[] args) {
-         String capabilities="{\"capabilities\":[{\"run\":\"no\",\"url\":\"http://127.0.0.1:4723/wd/hub\",\"platformName\":\"iOS\",\"autoAcceptAlerts\":\"true\",\"autoDismissAlerts\":\"true\",\"deviceName\":\"iPhone 6\",\"automationName\":\"XCUITest\",\"platformVersion\":\"10.3\",\"app\":\"/Users/pablomeseguer/Library/Developer/Xcode/DerivedData/PatientEngagement-bykbkgtwcohyuicjjmesihkdnaye/Build/Products/DaVitaHome_Debug-iphonesimulator/DaVitaHome.app\"},{\"run\":\"yes\",\"url\":\"http://127.0.0.1:4723/wd/hub\",\"platformName\":\"Android\",\"autoDismissAlerts\":\"true\",\"deviceName\":\"newEulator\",\"automationName\":\"Appium\",\"platformVersion\":\"5.1\",\"appActivity\":\"com.pe.login.LoginActivity\",\"appPackage\":\"com.pe.davita.debug\",\"app\":\"/Users/pablomeseguer/Desktop/Demogloo/PatientEngagement-Apperian.apk\"}]}";  String capabilities="{\"capabilities\":[{\"run\":\"no\",\"url\":\"http://127.0.0.1:4723/wd/hub\",\"platformName\":\"iOS\",\"autoAcceptAlerts\":\"true\",\"autoDismissAlerts\":\"true\",\"deviceName\":\"iPhone 6\",\"automationName\":\"XCUITest\",\"platformVersion\":\"10.3\",\"app\":\"/Users/pablomeseguer/Library/Developer/Xcode/DerivedData/PatientEngagement-bykbkgtwcohyuicjjmesihkdnaye/Build/Products/DaVitaHome_Debug-iphonesimulator/DaVitaHome.app\"},{\"run\":\"yes\",\"url\":\"http://127.0.0.1:4723/wd/hub\",\"platformName\":\"Android\",\"autoDismissAlerts\":\"true\",\"deviceName\":\"newEulator\",\"automationName\":\"Appium\",\"platformVersion\":\"5.1\",\"appActivity\":\"com.pe.login.LoginActivity\",\"appPackage\":\"com.pe.davita.debug\",\"app\":\"/Users/pablomeseguer/Desktop/Demogloo/PatientEngagement-Apperian.apk\"}]}";
-            Driver driver=new Driver(capabilities);
+        public static void main(String[] args) throws IOException {
+
+            String s;
+         //String capabilities="{\"capabilities\":[{\"run\":\"no\",\"url\":\"http://127.0.0.1:4723/wd/hub\",\"platformName\":\"iOS\",\"autoAcceptAlerts\":\"true\",\"autoDismissAlerts\":\"true\",\"deviceName\":\"iPhone 6\",\"automationName\":\"XCUITest\",\"platformVersion\":\"10.3\",\"app\":\"/Users/pablomeseguer/Library/Developer/Xcode/DerivedData/PatientEngagement-bykbkgtwcohyuicjjmesihkdnaye/Build/Products/DaVitaHome_Debug-iphonesimulator/DaVitaHome.app\"},{\"run\":\"yes\",\"url\":\"http://127.0.0.1:4723/wd/hub\",\"platformName\":\"Android\",\"autoDismissAlerts\":\"true\",\"deviceName\":\"newEulator\",\"automationName\":\"Appium\",\"platformVersion\":\"5.1\",\"appActivity\":\"com.pe.login.LoginActivity\",\"appPackage\":\"com.pe.davita.debug\",\"app\":\"/Users/pablomeseguer/Desktop/Demogloo/PatientEngagement-Apperian.apk\"}]}";  String capabilities="{\"capabilities\":[{\"run\":\"no\",\"url\":\"http://127.0.0.1:4723/wd/hub\",\"platformName\":\"iOS\",\"autoAcceptAlerts\":\"true\",\"autoDismissAlerts\":\"true\",\"deviceName\":\"iPhone 6\",\"automationName\":\"XCUITest\",\"platformVersion\":\"10.3\",\"app\":\"/Users/pablomeseguer/Library/Developer/Xcode/DerivedData/PatientEngagement-bykbkgtwcohyuicjjmesihkdnaye/Build/Products/DaVitaHome_Debug-iphonesimulator/DaVitaHome.app\"},{\"run\":\"yes\",\"url\":\"http://127.0.0.1:4723/wd/hub\",\"platformName\":\"Android\",\"autoDismissAlerts\":\"true\",\"deviceName\":\"newEulator\",\"automationName\":\"Appium\",\"platformVersion\":\"5.1\",\"appActivity\":\"com.pe.login.LoginActivity\",\"appPackage\":\"com.pe.davita.debug\",\"app\":\"/Users/pablomeseguer/Desktop/Demogloo/PatientEngagement-Apperian.apk\"}]}";
+            //Driver driver=new Driver(capabilities);
             try {
-                driver.androidDriverRun();
+           //     driver.androidDriverRun();
             } catch (Exception e) {
-                e.printStackTrace();
+             //   e.printStackTrace();
             }
+
+            Utilities u = new Utilities();
+            s = u.getCapabilities("config.properties");
+            System.out.println(s);
         }
 
 }
