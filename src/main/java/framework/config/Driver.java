64adcb926a654644f7b9c3443a3017013a3076fc
Pablo Meseguer <pablo.meseguer@davita.com>
2018-01-29 19:37:13 -0600

    Added capabilities Android

--- src/main/java/framework/config/Driver.java
+++ src/main/java/framework/config/Driver.java
@@ -1,4 +1,7 @@
 package framework.config;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import io.appium.java_client.remote.IOSMobileCapabilityType;
@@ -29,6 +32,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.internal.WrapsDriver;
 import org.openqa.selenium.remote.Response;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -39,6 +43,62 @@ import java.util.Set;
  */
 public class Driver {
 
+
+
+    public static AppiumDriver<WebElement> driver;
+    List<DesiredCapabilities> listCapabilities=null;
+    public static String capabilities;
+
+    public Driver() {
+        listCapabilities=generateCapabilities(capabilities);
+
+    }
+
+    public static  List<DesiredCapabilities> generateCapabilities(String capabilities){
+        List<DesiredCapabilities> desiredCapabilities = null;
+        JsonParser jsonObject = new JsonParser();
+        JsonObject jsonObjet = jsonObject.parse(capabilities).getAsJsonObject();
+        JsonObject jsonObjetAsJsonObject = jsonObjet.getAsJsonObject();
+        for(JsonElement jsonElement:jsonObjetAsJsonObject.get("capabilities").getAsJsonArray()){
+            DesiredCapabilities capabilitiesObjet=new DesiredCapabilities();
+            for(int i=0;i<=jsonElement.getAsJsonObject().entrySet().toArray().length-1;i++){
+          
+             String[] capabilityObject=   jsonElement.getAsJsonObject().entrySet().toArray()[i].toString().split("=");
+                if(capabilityObject[0].toString().contains("run")){
+
+                }
+                capabilitiesObjet.setCapability(capabilityObject[0].toString(),capabilityObject[1].toString());
+
+            }
+            desiredCapabilities.add(capabilitiesObjet);
+
+        }
+
+     return desiredCapabilities;
+    }
+
+
+
+    public  void androidCapabilities(){
+        for(int i=0;i<=listCapabilities.size();i++){
+           if(listCapabilities.get(i).getCapability("platformName").toString().contains("Android")){
+
+           }
+
+        }
+    }
+
+
+    public File getDirectoryApp(String directory , String appName){
+       try {
+           File appDir = new File(directory);
+           File app = new File(appDir, appName);
+           return app;
+       }
+       catch (Exception ex){
+           throw  ex;
+       }
+    }
 
 
 
--- src/main/java/framework/test.java
+++ src/main/java/framework/test.java
@@ -1,7 +1,18 @@
 package framework;
 
+import framework.config.Driver;
+
 /**
  * Created by pablomeseguer on 1/29/18.
  */
 public class test {
+
+
+
+        public static void main(String[] args) {
+          String capabilities="{\"capabilities\":[{\"run\":\"yes\",\"url\":\"http://127.0.0.1:4723/wd/hub\",\"platformName\":\"iOS\",\"autoAcceptAlerts\":\"true\",\"autoDismissAlerts\":\"true\",\"deviceName\":\"iPhone 6\",\"automationName\":\"XCUITest\",\"platformVersion\":\"10.3\",\"app\":\"/Users/pablomeseguer/Library/Developer/Xcode/DerivedData/PatientEngagement-bykbkgtwcohyuicjjmesihkdnaye/Build/Products/DaVitaHome_Debug-iphonesimulator/DaVitaHome.app\"},{\"run\":\"no\",\"url\":\"http://127.0.0.1:4723/wd/hub\",\"platformName\":\"Android\",\"autoDismissAlerts\":\"true\",\"deviceName\":\"newEulator\",\"automationName\":\"Appium\",\"platformVersion\":\"5.1\",\"appActivity\":\"com.pe.login.LoginActivity\",\"appPackage\":\"com.pe.davita.debug\",\"app\":\"/Users/pablomeseguer/Desktop/Demogloo/PatientEngagement-Apperian.apk\"}]}";
+            Driver driver=new Driver();
+            driver.generateCapabilities(capabilities);
+        }
+
 }
