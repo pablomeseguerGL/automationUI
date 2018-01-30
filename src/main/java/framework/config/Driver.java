2ceb82acbc908b5164bc5f3a5301308f19981caa
Pablo Meseguer <pablo.meseguer@davita.com>
2018-01-30 01:23:21 -0600

    Android driver added

--- src/main/java/framework/config/Driver.java
+++ src/main/java/framework/config/Driver.java
@@ -2,7 +2,11 @@ package framework.config;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServerHasNotBeenStartedLocallyException;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import io.appium.java_client.remote.IOSMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -33,74 +37,102 @@ import org.openqa.selenium.internal.WrapsDriver;
 import org.openqa.selenium.remote.Response;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 
+
+
+
+
 /**
  * Created by pablomeseguer on 1/29/18.
  */
-public class Driver {
+public class Driver  {
 
 
 
     public static AppiumDriver<WebElement> driver;
-    List<DesiredCapabilities> listCapabilities=null;
-    public static String capabilities;
 
-    public Driver() {
+    public static  ArrayList<DesiredCapabilities> listCapabilities=new ArrayList<>();
+
+
+
+    public Driver(String capabilities) {
+        AppiumDriverLocalService service = AppiumDriverLocalService.buildDefaultService();
+        service.start();
         listCapabilities=generateCapabilities(capabilities);
 
     }
 
-    public static  List<DesiredCapabilities> generateCapabilities(String capabilities){
-        List<DesiredCapabilities> desiredCapabilities = null;
+    public static ArrayList<DesiredCapabilities> generateCapabilities(String capabilities){
+
         JsonParser jsonObject = new JsonParser();
         JsonObject jsonObjet = jsonObject.parse(capabilities).getAsJsonObject();
         JsonObject jsonObjetAsJsonObject = jsonObjet.getAsJsonObject();
+        boolean execution=false;
         for(JsonElement jsonElement:jsonObjetAsJsonObject.get("capabilities").getAsJsonArray()){
             DesiredCapabilities capabilitiesObjet=new DesiredCapabilities();
             for(int i=0;i<=jsonElement.getAsJsonObject().entrySet().toArray().length-1;i++){
-          
+
              String[] capabilityObject=   jsonElement.getAsJsonObject().entrySet().toArray()[i].toString().split("=");
                 if(capabilityObject[0].toString().contains("run")){
+                    if(capabilityObject[1].toString().contains("yes")){
+                        execution=true;
+                    }
 
                 }
-                capabilitiesObjet.setCapability(capabilityObject[0].toString(),capabilityObject[1].toString());
+                if(execution && !capabilityObject[1].toString().contains("yes")){
+
+                    capabilitiesObjet.setCapability(capabilityObject[0].toString(),capabilityObject[1].toString().toString().replace("\"",""));
+
+                }
+
 
             }
-            desiredCapabilities.add(capabilitiesObjet);
+            if(execution){
+
+            listCapabilities.add(capabilitiesObjet);
+            }
+            execution=false;
 
         }
 
-     return desiredCapabilities;
+     return listCapabilities;
     }
 
-
-
-    public  void androidCapabilities(){
-        for(int i=0;i<=listCapabilities.size();i++){
-           if(listCapabilities.get(i).getCapability("platformName").toString().contains("Android")){
-
-           }
-
+public  ArrayList<DesiredCapabilities> searchCapability(String property, String value){
+    ArrayList<DesiredCapabilities> capabilities=new ArrayList<>();
+        for(DesiredCapabilities capability :listCapabilities){
+        if(capability.getCapability(property).toString().contains(value)){
+            capabilities.add(capability);
         }
+
     }
-
-
-    public File getDirectoryApp(String directory , String appName){
-       try {
-           File appDir = new File(directory);
-           File app = new File(appDir, appName);
-           return app;
-       }
-       catch (Exception ex){
-           throw  ex;
-       }
-    }
-
-
-
+    return capabilities;
 
 }
+
+    public  AndroidDriver androidDriverRun() throws Exception {
+        {
+
+
+  if(searchCapability("platformName","Android").size()>=1) {
+    DesiredCapabilities desiredCapabilities = searchCapability("deviceName", "gloo").get(0);
+    final String URL_STRING = "";
+    String urltemp = desiredCapabilities.getCapability("url").toString();
+    driver = new AndroidDriver<WebElement>(new URL(urltemp), desiredCapabilities);
+}
+        }
+
+   return (AndroidDriver) driver;
+
+        }
+
+    }
+
